395ace2458d9a9a9977667fc89244efaf4a7ad58
harmangrewal <grewal.harman.1989@gmail.com>
2016-09-03 11:39:18 -0400

    genral project3

---
+++ src/tutorial3/Application.java
@@ -0,0 +1,18 @@
+package tutorial3;
+
+public class Application {
+		public static void main(String[] args) {
+			int myInt = 7;
+			
+			String text = "Hello";
+			String blank = " ";
+			String name  = "Bob";
+			
+			String greeting = text + blank + name;
+			
+			
+			
+					System.out.println("hello" + " " + "Waheguruji");
+		}
+
+}
